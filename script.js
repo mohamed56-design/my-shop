adfde1a148a1b80bb4d0456d312f69d7c247ecfc
mohamed56-design <khaloufi.mohamed56@gmail.com>
2026-04-26 22:51:24 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-<script src="js/script.js"></script>
+function playVideo(src){
+    let modal = document.getElementById("videoModal");
+    let video = document.getElementById("videoPlayer");
+
+    video.src = src;
+    modal.style.display = "flex";
+    video.play();
+}
+
+function closeVideo(){
+    let modal = document.getElementById("videoModal");
+    let video = document.getElementById("videoPlayer");
+
+    video.pause();
+    modal.style.display = "none";
+}
