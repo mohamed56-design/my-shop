7f8b92fb6b50cb7c92844959cb8d64d610abe29e
mohamed56-design <khaloufi.mohamed56@gmail.com>
2026-04-26 23:34:32 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,48 +1,21 @@
 
+
 let videos = [
-  {
-    title: "Monkey Police Story",
-    url: "video1.mp4"
-  },
-  {
-    title: "Funny AI Street",
-    url: "video2.mp4"
-  }
+  "https://interactive-examples.mdn.mozilla.net/media/cc0-videos/flower.mp4"
 ];
 
 let index = 0;
 
 let player = document.getElementById("videoPlayer");
 
-function loadVideo(){
-    player.src = videos[index].url;
+function load(){
+  player.src = videos[index];
 }
 
 function nextVideo(){
-    index++;
-    if(index >= videos.length) index = 0;
-    loadVideo();
+  index++;
+  if(index >= videos.length) index = 0;
+  load();
 }
 
-function prevVideo(){
-    index--;
-    if(index < 0) index = videos.length - 1;
-    loadVideo();
-}
-
-function addVideo(){
-    let title = document.getElementById("title").value;
-    let url = document.getElementById("url").value;
-
-    videos.push({title, url});
-
-    document.getElementById("title").value = "";
-    document.getElementById("url").value = "";
-
-    alert("Video added 🔥");url: "https://interactive-examples.mdn.mozilla.net/media/cc0-videos/flower.mp4"
-  
-}
-
-// load first video
-loadVideo();
-
+load();
